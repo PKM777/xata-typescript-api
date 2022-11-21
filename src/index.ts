bcdcd39b288adfe7717bc759e37d38df77d5c936
Praveen K M <99317905+PKM777@users.noreply.github.com>
2022-11-21 18:51:50 +0530

    minor fix and update

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,29 @@ const app: Express = express();
 app.use(express.json());
 const port = process.env.PORT || 3000;
 
+app.get("/api/example", (req:Request, res:Response)=>{
+    res.json({msg:"hello get "})
+})
+
+app.post("/api/example", (req:Request, res:Response)=>{
+    res.json({msg:"hello post "})
+})
+
+app.put("/api/example/:id", (req:Request, res:Response)=>{
+    res.json({msg:"hello put "})
+})
+
+
+app.delete("/api/example/:id", (req:Request, res:Response)=>{
+    res.json({msg:"hello delete "})
+})
+
+
+
+
+
+
+
 
 app.listen(port, ()=>{
     console.log("server is running on port 3000")
