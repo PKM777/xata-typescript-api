ae7fe34c310d32531e5f201c9dc9b8c3a1e12daa
Praveen K M <99317905+PKM777@users.noreply.github.com>
2022-11-23 12:58:01 +0530

    connnecting to xata database and testing api

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,38 @@
 import express, { Express, Request, Response, urlencoded } from 'express';
 import dotenv from 'dotenv';
+import { getXataClient, Example  } from './xata';
+dotenv.config()
 
+
+const xata = getXataClient()
 const app: Express = express();
 app.use(express.json());
 const port = process.env.PORT || 3000;
 
-app.get("/api/example", (req:Request, res:Response)=>{
-    res.json({msg:"hello get "})
+app.get("/api/example", async (req:Request, res:Response)=>{
+    const exData = await xata.db.example.getAll()
+    res.json(exData)
 })
 
-app.post("/api/example", (req:Request, res:Response)=>{
-    res.json({msg:"hello post "})
+app.post("/api/example", async (req:Request, res:Response)=>{
+    const theData = req.body
+    const storeData = await xata.db.example.create(theData)
+
+    res.json(storeData)
 })
 
-app.put("/api/example/:id", (req:Request, res:Response)=>{
-    res.json({msg:"hello put "})
+app.put("/api/example/:id", async (req:Request, res:Response)=>{
+    const id = req.params.id
+    const examples = req.body
+    const updateData = await xata.db.example.update(id, examples)
+    res.json(updateData)
 })
 
 
-app.delete("/api/example/:id", (req:Request, res:Response)=>{
-    res.json({msg:"hello delete "})
+app.delete("/api/example/:id", async (req:Request, res:Response)=>{
+    const id = req.params.id
+    const deleteData = await xata.db.example.delete(id)
+    res.json(deleteData)
 })
 
 
